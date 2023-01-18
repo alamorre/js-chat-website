87845b36e4b6040ef792321958113c9fcf5182ba
Adam La Morre <alamorre@gmail.com>
2023-01-17 20:20:14 -0800

    New css stuff

--- src/ChatsPage.jsx
+++ src/ChatsPage.jsx
@@ -3,7 +3,7 @@ import { PrettyChatWindow } from 'react-chat-engine-pretty';
 const ChatsPage = (props) => {
     return (
         <div className="background">
-            <div style={{ height: '80%', width: '80%', marginLeft: '10%', color: 'white', background: "rgb(43, 47, 60)" }}>
+            <div className='chat-wrapper'>
                 <PrettyChatWindow
                     projectId={import.meta.env.VITE_CHAT_ENGINE_PROJECT_ID}
                     username={props.user.username}
